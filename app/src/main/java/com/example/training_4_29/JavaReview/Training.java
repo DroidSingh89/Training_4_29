ef211757a18971bba9254182a11a2ef9b178cfdf
DroidSingh89 <manroop.singh@mobileappscompany.com>
2019-04-30 11:32:23 -0400

    add flow controls

---
+++ app/src/main/java/com/example/training_4_29/JavaReview/Training.java
@@ -0,0 +1,8 @@
+package com.example.training_4_29.JavaReview;
+
+public class Training {
+
+    public static void main(String[] args) {
+
+    }
+}
--- app/src/main/java/com/example/training_4_29/Training.kt
+++
@@ -1,2 +0,0 @@
-package com.example.training_4_29
-

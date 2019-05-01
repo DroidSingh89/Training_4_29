133962319af23681617e3559c596d62359c24069
DroidSingh89 <manroop.singh@mobileappscompany.com>
2019-05-01 17:23:51 -0400

    testing new change

--- app/src/main/java/com/example/training_4_29/MainActivity.kt
+++ app/src/main/java/com/example/training_4_29/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    //testing change
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

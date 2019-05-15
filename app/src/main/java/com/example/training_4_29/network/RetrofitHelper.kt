4f3a404e65d3bdf821992dab07568199a7a3c815
DroidSingh89 <manroop.singh@mobileappscompany.com>
2019-05-15 09:45:15 -0400

    dynamic url

--- app/src/main/java/com/example/training_4_29/network/RetrofitHelper.kt
+++ app/src/main/java/com/example/training_4_29/network/RetrofitHelper.kt
@@ -8,11 +8,16 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
+import retrofit2.http.QueryMap
 
 class RetrofitHelper {
 
     private val baseUrl = "https://randomuser.me/"
+    private val path = "api"
+
+    private val queryMap = HashMap<String, String>()
+
     private fun createClient(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
@@ -26,7 +31,12 @@ class RetrofitHelper {
 
     fun getUserList(resultCount: String, callback: (RandomUserResponse)-> Unit){
 
-        createService().getUserList(resultCount).enqueue(object: Callback<RandomUserResponse>{
+        queryMap.let {
+            it.put("results", resultCount)
+            it.put("gender", "male")
+            it.put("nat", "US")
+        }
+        createService().getUserList(path, queryMap).enqueue(object: Callback<RandomUserResponse>{
             override fun onFailure(call: Call<RandomUserResponse>, t: Throwable) {
                 Log.d(RetrofitHelper::class.java.simpleName, "onFailure: ")
             }
@@ -41,7 +51,7 @@ class RetrofitHelper {
 
     interface RetrofitService{
 
-        @GET("api")
-        fun getUserList(@Query("results") result: String): Call<RandomUserResponse>
+        @GET("{custompath}")
+        fun getUserList(@Path("custompath") path: String, @QueryMap map: HashMap<String, String>): Call<RandomUserResponse>
     }
 }
